b6a9fca6569cd3d88a29d2131bf43ebb12a833f6
huntersli <sfinnerty@me.com>
2021-07-01 21:53:59 +0100

    Update Convert.ps1

--- Convert.ps1
+++ Convert.ps1
@@ -1,16 +1,17 @@
+Clear
 function Get-pdfs{
 param (
        $pdflocation
     )
-Write-host 'Testing PDF Location is accessable' 
+Write-Host $(Get-Date) 'Testing PDF Location is accessable' 
 If (Test-Path -Path $pdflocation){
    
-Write-host 'PDF Location is accessable' 
+Write-Host $(Get-Date) 'PDF Location is accessable' 
 Get-ChildItem -Path $pdflocation -Recurse -File | Where-Object {$_.Extension -eq ".pdf"}
 
 }Else{
 
-Write-Host 'Path is not avialable' 
+Write-Host $(Get-Date) 'Path is not avialable' 
 }
 
 
@@ -19,7 +20,7 @@ Write-Host 'Path is not avialable'
 
 ###Global Variables
 
-$global:pdffiles = Get-pdfs -pdflocation '\\finngeekfile003.finngeek.com\Personal\Shauns Documents\Personal\Bills and letters\Misc\'
+$global:pdffiles = Get-pdfs -pdflocation '\\finngeekfile003.finngeek.com\Personal\Shauns Documents\'
 $global:temppath = "C:\Temp\"
 
 function ConvertPNG{
@@ -29,7 +30,7 @@ param (
 
 if (Test-Path -Path $global:temppath) {
     } else {
-    Write-host Creating Temp Folder
+    Write-Host $(Get-Date) Creating Temp Folder
     New-Item -ItemType directory -Path $global:temppath
 }
 
@@ -42,11 +43,11 @@ if (Test-Path -Path $global:temppath) {
     $args = 'convert', '-density','150' + $argInput + $argOutpu
 
 
-Write-Host 'Converting ....' + $argInput + 'to PNG'
+Write-Host $(Get-Date) $(Get-Date) 'Converting ....' + $argInput + 'to PNG'
 
 & $magick $args 
 
-ConverttoPDF  -originalpath $pdf.DirectoryName -oringalName $pdf.Name
+ConverttoPDF  -pdf $pdf
 
 
 }
@@ -56,67 +57,95 @@ ConverttoPDF  -originalpath $pdf.DirectoryName -oringalName $pdf.Name
 function ConverttoPDF{
 param (
        
-       $originalpath,
-       $oringalName,
        $pdf
     )
 
-Write-Host 'Detecting Text and converting each PNG to PDF'    
+Write-Host $(Get-Date) 'Detecting Text and converting each PNG to PDF'    
 Get-ChildItem -Path $global:temppath -Filter '*.png' | ForEach-Object {
 Set-Location $global:temppath
-Write-Host 'Detecting Text and making ....' + $_.Name + 'to OCRed PDF'
-   & 'tesseract.exe' $_.Name ($_.BaseName) PDF
-    
+$teseractname = $_.Name
+$teseractbasename = $_.BaseName
+Write-Host $(Get-Date) 'Detecting Text and making ....' + $_.Name + 'to OCRed PDF'
+
+
+$args = "-l eng PDF"
+$teseractnameargs = '"' + $teseractname + '"'
+$teseractnameargs2 = '"' + $teseractbasename + '"'
+$teseractnameargs3 = "$args"
+
+
+
+
+try {
+   &'C:\Program Files\Tesseract-OCR\tesseract.exe'  $teseractnameargs $teseractnameargs2 PDF
+
+} catch {
+
+$string_err = $_ | Out-String
+
+}
+
+
   }
 
-Write-Host 'Merging All PDFS in Temp Folder'
-
+Write-Host $(Get-Date) 'Merging All PDFS in Temp Folder'
 
 
 $pdfext = '*.pdf'
 $outputpdf = 'out.pdf'
 $qpdfinputlocation = '"' + $global:temppath + $pdfext + '"'
 $qpdfoutputlocation = '"' + $global:temppath + $outputpdf + '"'
+$testpath = $global:temppath + $outputpdf
 
-& 'C:\qpdf\bin\qpdf.exe' --empty --pages $temppathargs -- $qpdfoutputlocation
+& 'C:\qpdf\bin\qpdf.exe' --empty --pages $qpdfinputlocation -- $qpdfoutputlocation
 
-
-$convertedpdf = Get-ChildItem -Path $global:temppath -Filter 'out.pdf'
-
-
-$originalpath = $pdf.fullname
-$movingLocation = $originalpath
-$backupname = $pdf.Name + '.old'
-
-
-Write-Host 'Renaming Orngial File - Location: ' $originalpath 
-Rename-Item -Path $originalpath -NewName $backupname
+Start-Sleep -s 5
+    If (Test-Path -Path $testpath ){
+            $originalpath = $pdf.fullname
+            $movingLocation = $pdf.fullname
+            $backupname = $pdf.Name + '.old'
 
 
-Write-Host 'Moving Converted PDF to Orginal Location'
-Move-Item -Path $convertedpdf.FullName -Destination $movingLocation
+            Write-Host $(Get-Date) 'Renaming Orngial File - Location: ' + $pdf.fullname  
+            #Rename-Item -Path $pdf.fullname -NewName $backupname
 
-$removepath = $global:temppath + '*.*'
-Write-Host 'remove unused Files'
-Write-Host 'Removing Backup File'
-Remove-Item $backupname
-Write-Host 'Removing Temp Files'
-Remove-Item $removepath
+            Write-Host $(Get-Date) 'Moving Converted PDF to Orginal Location :' $convertedpdf.FullName
+            Copy-Item -Path $testpath -Destination $movingLocation
+            Write-Host $(Get-Date) 'Movning Location :' $movingLocation
+
+
+            $removepath = $global:temppath + '*.*'
+            Write-Host $(Get-Date) 'Removing unused Files'
+
+           # Write-Host $(Get-Date) 'Removing Backup File'
+           # $backupItem = Get-ChildItem -Path $pdf.DirectoryName -Recurse -File | Where-Object {$_.Extension -eq ".old"}
+           # Remove-Item $backupItem.Name
+
+            Write-Host $(Get-Date) 'Removing Temp Files'
+            Remove-Item $removepath
+
+
+}Else {
+
+Write-Host 'Somethings Gone Wrong and qpdf has not outputted a file file or it cannot be found!' 
+
 }
 
 
+}
+
 foreach ($pdf in $global:pdffiles) {
-Write-Host 'Starting......'
-Write-Host 'Working on' $pdf.Name
+Write-Host $(Get-Date) 'Starting......'
+Write-Host $(Get-Date) 'Working on' $pdf.Name
 
 If ($pdf.Length -lt 25MB) {
-write-host $pdf.Name 'Is the right size' 
-write-host 'Converting PDF to PNG'
+Write-Host $(Get-Date) $pdf.Name 'Is the right size' 
+Write-Host $(Get-Date) 'Converting PDF to PNG'
 convertPNG -pdf $pdf 
 
 }else {
 
-write-host $pdf.Name 'is to big Skipping to next one' 
+Write-Host $(Get-Date) $pdf.Name 'is to big Skipping to next one' 
 
 }
 
